58c67b795543bab3f57ab816f01891df4a7719d2
denermiranda <dener@fuelling.club>
2016-12-09 23:32:59 -0300

    Adding messages support

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+	def show
+	end
+
+	def new
+		@message = Message.new
+		@users = User.all
+		@users.delete(current_user)
+	end
+
+	def index
+		@sent_messages = current_user.sent_messages
+		@received_messages = current_user.received_messages
+	end
+
+	def create
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get  '/newmessage',  to: 'messages#new'
   resources :users
+  resources :messages
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
