c80b7238c6d71a8b126372a302fe4f78385a1897
denermiranda <dener@fuelling.club>
2016-12-10 01:40:31 -0300

    Final Version

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,13 @@
 class MessagesController < ApplicationController
 	def show
+		@message = Message.find(params[:id])
 	end
 
 	def new
 		@message = Message.new
-		@users = User.all
+		@users = User.all.compact
+		@users.delete(current_user)
+		
 	end
 
 	def index
@@ -13,5 +16,18 @@ class MessagesController < ApplicationController
 	end
 
 	def create
+		@message = Message.new(user_params)
+		if @message.save
+			flash[:success] = "Sent!"
+      		redirect_to @message
+      	else
+      		render 'new'
+      	end
 	end
+
+	private
+
+	    def user_params
+	      params.require(:message).permit(:title, :content, :receiver_id, :sender_id)
+	    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :sender, :class_name => "User"
   belongs_to :receiver, :class_name => "User"
+  
+  validates :receiver_id, presence: true
+  validates :sender_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
